997d936907bc34bbba688c61e87c819620104a8e
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-05-14 11:33:07 -0700

    loaded image link onto map.html and it directs us to quest.html

--- src/map/map.js
+++ src/map/map.js
@@ -1,3 +1,11 @@
-//TODO: link BUTTON CLICK EVENT'S VALUE t
-
 import createQuestLink from './create-quest-link.js';
+import api from '../api.js';
+
+const parentElement = document.getElementById('parent-element');
+const quests = api.getQuests();
+
+for (let i = 0; i < quests.length; i++) {
+    const quest = quests[i];
+    const link = createQuestLink(quest);
+    parentElement.appendChild(link);
+}
